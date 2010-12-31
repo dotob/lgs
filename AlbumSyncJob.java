553d6b3c18f146797f111d09a9accc12e23ca864
basti <sk@dotob.de>
2010-12-31 18:33:13 +0100

    minor steps toward syncjob intellij 10

--- AlbumSyncJob.java
+++ AlbumSyncJob.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.Vector;
+import java.util.concurrent.ExecutionException;
 
 public class AlbumSyncJob implements ActionListener, PropertyChangeListener {
     Timer checkTimer;
@@ -40,15 +41,58 @@ public class AlbumSyncJob implements ActionListener, PropertyChangeListener {
         // check if albumfetching is in progress, then skip this timer, but tell someone
         if (albumProvider != null && !albumProvider.isDone()) {
             output.showMessage("letzter album check noch nicht fertig, überspringe dieses mal");
+        } else {
+            // ok albumprovider is null then create a new one and check
+            this.albumProvider = new BaseAlbumProvider(this.output);
+            this.albumProvider.addPropertyChangeListener(this);
+            this.albumProvider.execute();
         }
-        // ok albumprovider is null then create a new one and check
-        this.albumProvider = new BaseAlbumProvider(this.output);
-        this.albumProvider.addPropertyChangeListener(this);
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
         // this is for the albumprovider
-        
+        if (this.albumProvider != null && this.albumProvider.isDone()) {
+            // check if this is first time
+            if (this.lastSyncAlbums == null) {
+                doFirstTimeCheck();
+            } else {
+                doCheck4NewAlbums();
+            }
+        }
+    }
+
+    private void doCheck4NewAlbums() {
+        Vector<Album> albums = new Vector<Album>();
+        try {
+            albums = this.albumProvider.get();
+        } catch (InterruptedException e) {
+            this.output.showMessage("something bad happened... " + e.getMessage());
+        } catch (ExecutionException e) {
+            this.output.showMessage("something bad happened... " + e.getMessage());
+        }
+        Vector<Album> todoList = new Vector<Album>();
+        // compare album vectors
+        for (Album a : albums) {
+            if (!this.lastSyncAlbums.contains(a)) {
+                // add it to todolist
+                todoList.add(a);
+                this.output.showMessage("album " + a.getName() + " mit " + a.getCount() + " bildern zur todoliste gefügt");
+            }
+        }
+        if (todoList.size() > 0) {
+            //TODO:  do the action
+            
+        }
+    }
+
+    private void doFirstTimeCheck() {
+        try {
+            this.lastSyncAlbums = this.albumProvider.get();
+        } catch (InterruptedException e) {
+            this.output.showMessage("something bad happened... " + e.getMessage());
+        } catch (ExecutionException e) {
+            this.output.showMessage("something bad happened... " + e.getMessage());
+        }
     }
 }
